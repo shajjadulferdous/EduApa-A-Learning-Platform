a73d1136137397ff5a4e8e85b8d31323b249de05
Md Shajjadul Ferdous <shajjadulferdous@gmail.com>
2026-05-02 01:16:46 +0600

    Register Page Added

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
-
+import logo from "@/asset/eduapa.png"
+import Image from 'next/image';
+import Link from 'next/link';
 const RegisterPage = () => {
     return (
-        <div>
-            Hello register page
+        <div className='h-screen flex justify-center items-center flex-col'>
+             <div>
+                <Image src={logo} alt="logo" width={120} height={120} className='grayscale-100'></Image>
+             </div>
+            <h1 className='font-semibold text-xl text-gray-600'>Sign up for EduApa</h1>
+            <div>
+                <fieldset className="fieldset rounded-box w-100 p-4">       
+
+                <label className="label text-gray-600 font-semibold text-[16px]">Email</label>
+                <input type="email" className="input outline-none w-100 hover:border-blue-500 hover:border-2" placeholder="Email" />
+              
+                <label className="label text-gray-600 font-semibold text-[16px]">Name</label>
+                <input type="text" className="input outline-none w-100 hover:border-blue-500 hover:border-2" placeholder="Your Name" />
+                 
+                <label className="label text-gray-600 font-semibold text-[16px]">Photo</label>
+                <input type="text" className="input outline-none w-100 hover:border-blue-500 hover:border-2" placeholder="URL" />
+
+                <label className="label text-gray-600 font-semibold text-[16px]">Password</label>
+                <input type="password" className="input outline-none w-100 hover:border-blue-500 hover:border-2" placeholder="Password" />
+
+                <button className="btn bg-green-900 text-white mt-4">Create Account</button>
+                </fieldset>
+                <p className='flex justify-center'>---------------or---------------</p>
+                <div className='flex justify-center mt-5'>
+                    <button className="btn  text-black bg-base-200 w-100 border-[#e5e5e5]">
+                    <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+                     Continue with Google
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
