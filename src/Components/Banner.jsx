93ef762cdad24a0a9b19b48f98d01f71c6b3ab72
Md Shajjadul Ferdous <shajjadulferdous@gmail.com>
2026-05-02 08:34:54 +0600

    animation added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,12 +4,12 @@ import sibling from "@/asset/04.png"
 const Banner = () => {
     return (
         <div className='flex w-11/12 mx-auto flex-col-reverse md:flex-row'>
-            <div className='flex-1 flex justify-center items-center'>
-                <Image src={sibling} alt="an image" width={500} height={400} />
+            <div className='flex-1 flex justify-center items-center '>
+                <Image src={sibling} alt="an image" width={500} height={400} className='animate-[bounce_20s_ease-in-out_infinite]' />
             </div>
             <div className='flex-1 flex items-center justify-center mt-10 md:mt-0'>
                <div className=''>
-                <h1 className='font-bold text-5xl text-center mb-3'>Smart Preparation for Smart Students</h1>
+                <h1 className='font-bold text-5xl text-center mb-3 animate__animated animate__bounce animate__delay-2s'>Smart Preparation for Smart Students</h1>
                 <p className='text-center'>Making complex topics easy to understand.
                 Start your journey today.</p>
                </div>
