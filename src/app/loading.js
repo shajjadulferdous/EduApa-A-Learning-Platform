ef99b206c6dfeb80550dce0c99fbfff120617826
Md Shajjadul Ferdous <shajjadulferdous@gmail.com>
2026-05-02 14:49:40 +0600

    auth-client

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,4 @@
 export default function Loading() {
-  // Or a custom loading skeleton component
   return (
      <div className="h-screen flex items-center justify-center">
          <span className="loading loading-spinner loading-xl"></span>
